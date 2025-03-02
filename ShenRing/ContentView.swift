a203cc627f68d6dfa1050052389493704bac7b09
FLAIVIINGO <ababilon@gmu.edu>
2025-03-02 14:35:24 -0500

    Initial Commit

---
+++ ShenRing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShenRing
+//
+//  Created by Andrew Babilonia on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShenRing/ShenRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShenRingApp.swift
+//  ShenRing
+//
+//  Created by Andrew Babilonia on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ShenRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
