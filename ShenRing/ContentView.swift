1ec5428f05ba88ca02226161e1ad0eea9f5f412e
FLAIVIINGO <ababilon@gmu.edu>
2025-03-03 21:44:16 -0500

    card developments. begin mvvm architecture layout

--- ShenRing/ContentView.swift
+++ ShenRing/ContentView.swift
@@ -8,23 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    let hieros = ["𓃲","𓅋","𓆣","𓏢"]
+    
     var body: some View {
         VStack {
-            CardView()
+            cards
         }
-        .padding()
+        .padding(16)
+    }
+    
+    var cards: some View {
+        LazyVGrid(
+            columns: [GridItem(.flexible(), spacing: 16), GridItem(.flexible(), spacing: 16)], spacing: 16) {
+            ForEach(0..<4, id: \.self) { index in
+                CardView(content: hieros[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
     }
 }
 
 struct CardView: View {
+    @State var isSelected = false
+    let content: String
     
     var body: some View {
         let base = RoundedRectangle(cornerRadius: 12)
         ZStack {
             base.foregroundColor(.white)
             base.stroke(lineWidth: 2)
-            Text("🐼")
+                .foregroundColor(isSelected ? .blue : .black)
+            Text(content)
                 .font(.largeTitle)
+                .foregroundColor(isSelected ? .blue : .black)
+        }
+        .onTapGesture {
+            isSelected.toggle()
         }
     }
 }
